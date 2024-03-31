803875dc6c1895f6d248170dfc5c57b9fa4c2a1d
Prabodhani Karunarathne <karunarathneprabodhani@gmail.com>
2024-03-31 15:27:33 +0530

    Updated in PrabodhaniBranch

--- NaviGuideFrontEnd/naviguide/src/App.js
+++ NaviGuideFrontEnd/naviguide/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import { useEffect } from 'react';
+import {Tooltip,initTE} from "tw-elements";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './Components/LoginPage/LoginPage';
 import RegisterForm from './Components/RegisterPage/RegisterPage';
 import ProfilePage from './Components/Profile/ProfilePage';
 import { AuthProvider } from './AuthContext';
+import RatingUser from './Components/RatingUser/RatingUser';
 
 const App = () => {
+
+  useEffect(() => {
+    initTE({ Tooltip });
+  }, []);
+
   return (
     <Router>
       {/*Wrapped Routes with authprovider */}
@@ -18,10 +25,15 @@ const App = () => {
           <Route path="/register" element={<RegisterForm />} />
           
           <Route path="/ProfilePage" element={<AuthProvider><ProfilePage/></AuthProvider>}/>
+
+          <Route path="/ratingUser" element={<RatingUser />} />
         
         
         </Routes>
     </Router>
+    
+  
+    
   );
 };
 
---
+++ NaviGuideFrontEnd/naviguide/src/Components/RatingUser/RatingUser.js
@@ -0,0 +1,140 @@
+// import React from "react";
+// import "./RatingUser.css";
+// import {Rating,initTE,} from "tw-elements";
+// const RatingUser=() => {
+    
+    // TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
+// Initialization for ES Users
+
+  
+//   initTE({ Rating });
+    
+    
+//     return(
+// <div id="ratingBack">
+
+// <ul class="my-1 flex list-none gap-1 p-0 " data-te-rating-init>
+//   <li>
+//     <span
+//       class="text-[#673ab7] [&>svg]:h-5 [&>svg]:w-5"
+//       data-te-rating-icon-ref>
+//       <svg
+//         xmlns="http://www.w3.org/2000/svg"
+//         fill="none"
+//         viewBox="0 0 24 24"
+//         stroke-width="1.5"
+//         stroke="currentColor">
+//         <path
+//           stroke-linecap="round"
+//           stroke-linejoin="round"
+//           d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+//       </svg>
+//     </span>
+//   </li>
+//   <li>
+//     <span
+//       class="text-[#3f51b5] [&>svg]:h-5 [&>svg]:w-5"
+//       data-te-rating-icon-ref>
+// <svg
+//   xmlns="http://www.w3.org/2000/svg"
+//   fill="none"
+//   viewBox="0 0 10 20" // Adjust the viewBox to set the desired size
+//   width="100" // Set the width to maintain aspect ratio
+//   height="20" // Set the height
+//   stroke-width="1.5"
+//   stroke="currentColor">
+//   <path
+//     stroke-linecap="round"
+//     stroke-linejoin="round"
+//     d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+// </svg>
+
+
+//     </span>
+//   </li>
+//   <li>
+//     <span
+//       class="text-[#2196f3] [&>svg]:h-5 [&>svg]:w-5"
+//       data-te-rating-icon-ref>
+//       <svg
+//         xmlns="http://www.w3.org/2000/svg"
+//         fill="none"
+//         viewBox="0 0 24 24"
+//         stroke-width="1.5"
+//         stroke="currentColor">
+//         <path
+//           stroke-linecap="round"
+//           stroke-linejoin="round"
+//           d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+//       </svg>
+//     </span>
+//   </li>
+//   <li>
+//     <span
+//       class="text-[#03a9f4] [&>svg]:h-5 [&>svg]:w-5"
+//       data-te-rating-icon-ref>
+//       <svg
+//         xmlns="http://www.w3.org/2000/svg"
+//         fill="none"
+//         viewBox="0 0 24 24"
+//         stroke-width="1.5"
+//         stroke="currentColor">
+//         <path
+//           stroke-linecap="round"
+//           stroke-linejoin="round"
+//           d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+//       </svg>
+//     </span>
+//   </li>
+//   <li>
+//     <span
+//       class="text-[#00bcd4] [&>svg]:h-5 [&>svg]:w-5"
+//       data-te-rating-icon-ref>
+//       <svg
+//         xmlns="http://www.w3.org/2000/svg"
+//         fill="none"
+//         viewBox="0 0 24 24"
+//         stroke-width="1.5"
+//         stroke="currentColor">
+//         <path
+//           stroke-linecap="round"
+//           stroke-linejoin="round"
+//           d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+//       </svg>
+//     </span>
+//   </li>
+// </ul>
+// </div>
+//     );
+// }
+import React from "react";
+import "./RatingUser.css";
+
+const RatingUser = () => {
+  return (
+    <div id="ratingBack">
+      <ul className="my-1 flex list-none gap-1 p-0">
+        <li>
+          <span className="star">&#9733;</span>
+        </li>
+        <li>
+          <span className="star">&#9733;</span>
+        </li>
+        <li>
+          <span className="star">&#9733;</span>
+        </li>
+        <li>
+          <span className="star">&#9733;</span>
+        </li>
+        <li>
+          <span className="star">&#9733;</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+//export default RatingUser;
+
+
+export default RatingUser;
