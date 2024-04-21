4206ce58472274653358c46eae549c06bf9dfb27
Prabodhani Karunarathne <karunar-ps19204@stu.kln.ac.lk>
2024-04-21 22:38:05 +0530

    Merge Navigation Bar

--- NaviGuideFrontEnd/naviguide/src/Components/NavigationBar/NavigationBar.js
+++ NaviGuideFrontEnd/naviguide/src/Components/NavigationBar/NavigationBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./NavigationBar.css" ;
 import { Link } from "react-router-dom";
-import HomePage from "../HomePage/HomePage";
+
 
 
 
@@ -9,16 +9,27 @@ const NavigationBar = () =>{
     return(
         
         <div className="navBar">
-            <ul>
-            <li><Link to={HomePage} className="link">HOME</Link></li>
-            <li><Link to={'*'} className="link">ABOUT</Link></li>
-            <li><Link to={'*'} className="link">CONTACT</Link></li>
-            <li><Link to={'*'} className="link">SERVICES</Link></li>
-            </ul>
+            <div class="div1">
+                <ul className="listnav">
+                    <li ><Link to={""} className="link">HOME</Link></li>
+                    <li ><Link to={""} onClick="aboutClick" className="link" >ABOUT</Link></li>
+                    <li ><Link to={"*"} className="link">NEWS</Link></li>
+                    <li ><Link to={"*"} className="link">CONTACT</Link></li>
+                 </ul>
+            </div>
 
+            <div class="div2">
+              
+                <button class="login"><Link to={"*"} className="link1">Login</Link></button>
+                <button class="signup"><Link to={"*"} className="link1">Signup</Link></button>
+            
+            </div>
+
+            
+              
         </div>
     );
-
+    
 }
 
-export default NavigationBar;
+export default NavigationBar;
